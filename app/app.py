f9976cfedb0697d2a38aa9467e0a65a3584c2bd4
prasad-m-k <prasad_mk@outlook.com>
2025-09-17 07:31:05 -0700

    .

--- app/app.py
+++ app/app.py
@@ -363,6 +363,6 @@ if __name__ == '__main__':
             '/etc/letsencrypt/live/qaproposalmusicsciencegroup.com/privkey.pem'
         ))
     else:
-        app.run(port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=True)
     """
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
